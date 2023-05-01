cd84407f00c8eb49761729702352f952e2d96259
Igor <igorplan3@gmail.com>
2023-05-01 20:18:11 +0300

    first page with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DetailCharacter from './Components/detail-character/detail-character'
 import MainInfo from './Components/main-info/main-info';
 import  ComicsList  from './Components/comics list/comics-list';
 import ComicsDetail from './Components/comics-detail/comics-detail';
 
-export default class MarvelComics extends Component {
+export default function  MarvelComics() {
   state = {
     page: 'start-page',
     selectedCard: false,
@@ -13,37 +14,21 @@ export default class MarvelComics extends Component {
     searchWasDone: '',
   }
 
- 
-  render() {
-    let { page }  = this.state;
-    let ShowInfo = '';
-    if(page === 'start-page') {
-      ShowInfo = <MainInfo/>
-    }
-    if(page === 'detail-info') {
-      ShowInfo = <DetailCharacter/>
-    }
-    if(page === 'comics-list') {
-      ShowInfo = <ComicsList/>
-
-    }
-    if(page === 'comics-detail'){
-      ShowInfo = <ComicsDetail/>
-    }
-
-   
-
     return (
-      <div className="wrapper">
+     <Router>
+         <div className="wrapper">
         <div className="main-page">
           <div className="main-page__top-label top-label">
             <span className='top-label__item-left'><span>Marvel</span> information portal</span>
             <span className='top-label__item-right'><span className='top-label__item-right__item'>
               Characters</span> / Comics</span>
           </div>
-          {ShowInfo}
+          <Route path='/' element={<MainInfo/>}/>
+          <Route path='detail-info' element={<DetailCharacter/>}/>
+          <Route path='comics-list' element={<ComicsList/>}/>
+          <Route path='comics-detail'element={<ComicsDetail/>}/>
         </div>
       </div>
+     </Router>
     )
   }
-}
